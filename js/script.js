b36458e7636da09b7414fc84ea983be0c6ead260
sdb07khan <ksaddab@gmail.com>
2023-06-02 19:46:15 +0530

    some more section of animation completed

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ window.addEventListener("load", () => {
   });
 
   circleRevealAnimation
-    .to(".circleAnimation", { scale: 1, duration: 0.5 })
+    .to(".circleAnimation", { scale: 1.5, duration: 0.5 })
     .to(".imageBox:nth-child(1)", { opacity: 0, duration: 0.5 }, "<")
     .to(".imageBox:nth-child(2)", { opacity: 1, duration: 0.5 }, "<")
     .to(".circleAnimation", { scale: 0.5, duration: 0.5 })
@@ -176,35 +176,36 @@ window.addEventListener("load", () => {
     animation: circleRevealAnimation,
     trigger: ".animationContainer",
     start: "top top",
-    end: "+=1200%",
+    end: "+=1300%",
     pin: ".animationContainer",
     toggleActions: "play reverse none reverse",
     scrub: 1,
   });
 
-  const squareRevealAnimation = gsap.timeline({ duration: 2, delay: -10 });
+  //Square Animation HIGHLIGHT
 
-  // squareRevealAnimation.set(".cloudTextBox1", {
-  //   autoAlpha: 0,
-  // });
+  const squareRevealAnimation = gsap.timeline({
+    duration: 10,
+    ease: "power2.out",
+  });
 
   squareRevealAnimation
     .to(".squareAnimation", { scale: 1.7 })
-    .to(".squareImageBox:nth-child(1)", { opacity: 0, delay: 3 }, "<")
+    .to(".squareImageBox:nth-child(1)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.6 })
-    .to(".squareImageBox:nth-child(2)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(2)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.5 })
-    .to(".squareImageBox:nth-child(3)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(3)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.45 })
-    .to(".squareImageBox:nth-child(4)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(4)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.4 })
-    .to(".squareImageBox:nth-child(5)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(5)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.3 })
-    .to(".squareImageBox:nth-child(6)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(6)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.25 })
-    .to(".squareImageBox:nth-child(7)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(7)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.22 })
-    .to(".squareImageBox:nth-child(8)", { opacity: 0 }, "<")
+    .to(".squareImageBox:nth-child(8)", { opacity: 0, duration: 0.4 }, "<")
     .to(".squareAnimation", { scale: 1.2 })
     .to(".cloudTextBox1", {
       zIndex: 0,
@@ -231,13 +232,17 @@ window.addEventListener("load", () => {
     .to(".squareAnimation", { scale: 0.9 })
     .to(".squareImageBox:nth-child(15)", { opacity: 0 }, "<")
     .to(".squareAnimation", { scale: 0.85 })
-    .to(".squareImageBox:nth-child(16)", { opacity: 0 }, "<")
+    .to(
+      ".squareImageBox:nth-child(16)",
+      { opacity: 0, ease: "power2.out", duration: 4 },
+      "<"
+    )
     .to(
       ".codePictureBox1",
       {
         zIndex: 1,
         opacity: 1,
-        // scrollTrigger: { trigger: ".codePictureBox1", start: "top center" },
+        ease: "power2.out",
       },
       "<"
     )
@@ -245,16 +250,70 @@ window.addEventListener("load", () => {
     .to(".codePictureBox1", {
       zIndex: -1,
       opacity: 0,
-      // scrollTrigger: { trigger: ".codePictureBox1", start: "top center" },
+      ease: "power2.out",
+      duration: 3,
+    })
+    .to(
+      ".microBox1",
+      {
+        zIndex: 0,
+        opacity: 1,
+        ease: "power2.out",
+      },
+      "<"
+    )
+    .to(".microBox3", { y: "-10%", delay: 2, duration: 8 }, "<")
+    .to(".microBox1", {
+      zIndex: -1,
+      opacity: 0,
+      ease: "power2.out",
+      duration: 3,
+    })
+    .to(
+      ".anyBox1",
+      {
+        zIndex: 0,
+        opacity: 1,
+        ease: "power2.out",
+        duration: 0.5,
+        delay: 3,
+      },
+      "<"
+    )
+
+    .to(".bottomLine", { opacity: 1, scaleX: 1, duration: 1 })
+    .to(".anyText1", { opacity: 1, x: 0, duration: 6 })
+    .to(".anyText2", { opacity: 1, x: 0, duration: 6 })
+    .to(".anyBox1", {
+      zIndex: -1,
+      opacity: 0,
+      ease: "power2.out",
+      duration: 0.5,
+    })
+    .to(
+      ".downloadBox1",
+      {
+        zIndex: 1,
+        opacity: 1,
+        ease: "power2.out",
+        duration: 3,
+      },
+      "<"
+    )
+    .to(".downloadBox3", { y: "-100%", duration: 30 })
+    .to(".downloadBox1", {
+      zIndex: -1,
+      ease: "power2.out",
+      duration: 0.5,
     });
 
   ScrollTrigger.create({
     animation: squareRevealAnimation,
     trigger: ".animationContainer",
     start: "top top",
-    end: "+=400%",
+    end: "+=1100%",
     // pin: ".animationContainer",
     toggleActions: "play reverse none reverse",
-    scrub: 1,
+    scrub: 0.5,
   });
 });
